175399568ad17189bc75093cdd6f9b401f227874
Your Name <mafevito@hotmail.com>
2017-11-23 13:43:03 +0100

    Nombres cambiados

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Socialblog
+# socialbg
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.3.
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('socialblog App', () => {
+describe('socialbg App', () => {
   let page: AppPage;
 
   beforeEach(() => {
